eb39b9f4b827de3c3f9863e9c9a4006bc1b38fe1
Caleb <caleb.schoepp@gmail.com>
2020-03-28 23:13:47 -0700

    Use snoowrap in scraper script

--- scripts/scrape_reddit.js
+++ scripts/scrape_reddit.js
@@ -1,4 +1,29 @@
-function sayHello() {
-  console.log("Hello");
+const snoowrap = require("snoowrap");
+
+async function run() {
+  const r = new snoowrap({
+    userAgent: process.env.REDDIT_USER_AGENT,
+    clientId: process.env.REDDIT_CLIENT_ID,
+    clientSecret: process.env.REDDIT_CLIENT_SECRET,
+    refreshToken: process.env.REDDIT_REFRESH_TOKEN
+  });
+
+  const subreddit = await r.getSubreddit("realEstate");
+  const topPosts = await subreddit.getTop({ time: "week", limit: 3 });
+
+  let data = [];
+
+  topPosts.forEach(post => {
+    data.push({
+      link: post.url,
+      text: post.title,
+      score: post.score
+    });
+  });
+
+  console.log(data);
 }
-sayHello();
+
+console.log(process.env);
+
+run();
